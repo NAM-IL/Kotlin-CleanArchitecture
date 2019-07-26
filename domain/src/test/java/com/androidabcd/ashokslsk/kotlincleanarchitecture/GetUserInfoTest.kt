a3528543035d117fab774e6218e55cac760a14ce
Srinivasa Ashok Kumar <ashokslsk@gmail.com>
2019-07-26 07:42:01 +0530

    Added a first Unit Test

---
+++ domain/src/test/java/com/androidabcd/ashokslsk/kotlincleanarchitecture/GetUserInfoTest.kt
@@ -0,0 +1,56 @@
+package com.androidabcd.ashokslsk.kotlincleanarchitecture
+
+import com.androidabcd.ashokslsk.kotlincleanarchitecture.repository.BankingRepository
+import com.androidabcd.ashokslsk.kotlincleanarchitecture.utils.TestDataGenerator
+import io.reactivex.Observable
+import io.reactivex.Scheduler
+import io.reactivex.schedulers.Schedulers
+import org.drulabs.bankbuddy.domain.usecases.GetUserInfoTask
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.Mockito.times
+import org.mockito.MockitoAnnotations
+
+/**
+ * Created by Srinivasa Ashok Kumar on 2019-07-26.
+ */
+@RunWith(JUnit4::class)
+class GetUserInfoTest {
+    private lateinit var getUserInfoTask: GetUserInfoTask
+
+    @Mock
+    private lateinit var bankingRepository: BankingRepository
+
+    @Before
+    fun setup(){
+        MockitoAnnotations.initMocks(this)
+        getUserInfoTask = GetUserInfoTask(
+            bankingRepository,
+            Schedulers.trampoline(),
+            Schedulers.trampoline()
+        )
+    }
+
+    @Test
+    fun test_getUserInfo_success(){
+        val userInfo = TestDataGenerator.generateUserInfo()
+
+        Mockito.`when`(bankingRepository.getUserInfo(userInfo.accountNumber))
+            .thenReturn(Observable.just(userInfo))
+
+        val testObserver = getUserInfoTask.buildUseCase(userInfo.accountNumber)
+            .test()
+
+        Mockito.verify(bankingRepository, times(1))
+            .getUserInfo(userInfo.accountNumber)
+
+        testObserver.assertSubscribed()
+            .assertValue {it == userInfo}
+            .assertComplete()
+
+    }
+}
